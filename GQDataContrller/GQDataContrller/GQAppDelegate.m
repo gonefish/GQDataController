d6d81edf641bbab57addbdbea54750e6fa055da4
Qian GuoQiang <gonefish@gmail.com>
2013-03-19 21:38:04 +0800

    更新版权信息及工程信息

--- GQDataContrller/GQDataContrller/GQAppDelegate.m
+++ GQDataContrller/GQDataContrller/GQAppDelegate.m
@@ -3,7 +3,7 @@
 //  GQDataContrller
 //
 //  Created by 钱国强 on 12-12-9.
-//  Copyright (c) 2012年 Qian GuoQiang. All rights reserved.
+//  Copyright (c) 2012年 gonefish@gmail.com. All rights reserved.
 //
 
 #import "GQAppDelegate.h"

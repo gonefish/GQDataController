caa11638ad612d6935bf8baca762f15c78871d7c
Qian GuoQiang <gonefish@gmail.com>
2015-06-13 16:39:59 +0800

    清理代码

--- GQDataController.h
+++ GQDataController.h
@@ -9,25 +9,9 @@
 #import <UIKit/UIKit.h>
 #import "GQDataControllerDelegate.h"
 
-typedef void (^GQTableViewCellConfigureBlock)(UITableViewCell *cell, id cellModel);
-
-typedef void (^GQCOllectionViewCellConfigureBlock)(UICollectionViewCell *cell, id cellModel);
-
 @interface GQDataController : NSObject
 
 @property (nonatomic, weak) id <GQDataControllerDelegate> delegate;
-
-@property (nonatomic, weak) id <UITableViewDataSource> tableViewDataSource;
-
-@property (nonatomic, copy) GQTableViewCellConfigureBlock tableViewCellConfigureBlock;
-
-@property (nonatomic, weak) id <UICollectionViewDataSource> collectionViewDataSource;
-
-@property (nonatomic, copy) GQCOllectionViewCellConfigureBlock collectionCellConfigureBlock;
-
-@property (nonatomic, strong, readonly) id detailObject;
-
-@property (nonatomic, strong, readonly) NSArray *listObjects;
 
 /**
  *  共享实例的方法，共享实例的请求队列是串行的
